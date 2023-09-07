e17b1e1dc1f229104f12b1bdaed2be13639cd1be
Guilherme Senci <guilherme@ephrom.com.br>
2023-09-07 13:31:16 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function () {
+  var linkVoltarAoTopo = document.querySelector(".voltar-ao-topo");
+  if (window.scrollY > 200) {
+    // Exibe o link quando a rolagem for maior que 200 pixels
+    linkVoltarAoTopo.style.display = "block";
+  } else {
+    linkVoltarAoTopo.style.display = "none";
+  }
+});
+
+document.title = "Dev Guilherme"
