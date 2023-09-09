ecdbe5cced6ae02cc13ef0128cbbc489469f304e
Guilherme Senci <guilherme@ephrom.com.br>
2023-09-09 02:49:06 -0300

    refactor: updating my portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-  var linkVoltarAoTopo = document.querySelector(".voltar-ao-topo");
-  if (window.scrollY > 200) {
-    // Exibe o link quando a rolagem for maior que 200 pixels
-    linkVoltarAoTopo.style.display = "block";
-  } else {
-    linkVoltarAoTopo.style.display = "none";
-  }
-});
